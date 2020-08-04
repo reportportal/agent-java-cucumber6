efe374f4d3123996262ef8d01095175d929b63cf
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-08-04 16:54:08 +0300

    TestItem description tests

---
+++ src/test/java/com/epam/reportportal/cucumber/FeatureDescriptionTest.java
@@ -0,0 +1,120 @@
+package com.epam.reportportal.cucumber;
+
+import com.epam.reportportal.cucumber.integration.TestScenarioReporter;
+import com.epam.reportportal.cucumber.integration.TestStepReporter;
+import com.epam.reportportal.cucumber.integration.util.TestUtils;
+import com.epam.reportportal.listeners.ListenerParameters;
+import com.epam.reportportal.service.ReportPortal;
+import com.epam.reportportal.service.ReportPortalClient;
+import com.epam.reportportal.util.test.CommonUtils;
+import com.epam.ta.reportportal.ws.model.StartTestItemRQ;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.startsWith;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author <a href="mailto:ihar_kahadouski@epam.com">Ihar Kahadouski</a>
+ */
+public class FeatureDescriptionTest {
+
+	@CucumberOptions(features = "src/test/resources/features/belly.feature", glue = {
+			"com.epam.reportportal.cucumber.integration.feature" }, plugin = { "pretty",
+			"com.epam.reportportal.cucumber.integration.TestScenarioReporter" })
+	public static class BellyScenarioReporter extends AbstractTestNGCucumberTests {
+
+	}
+
+	@CucumberOptions(features = "src/test/resources/features/belly.feature", glue = {
+			"com.epam.reportportal.cucumber.integration.feature" }, plugin = { "pretty",
+			"com.epam.reportportal.cucumber.integration.TestStepReporter" })
+	public static class BellyStepReporter extends AbstractTestNGCucumberTests {
+
+	}
+
+	private final String launchId = CommonUtils.namedId("launch_");
+	private final String suiteId = CommonUtils.namedId("suite_");
+	private final List<String> testIds = Stream.generate(() -> CommonUtils.namedId("test_")).limit(3).collect(Collectors.toList());
+	private final List<Pair<String, ? extends Collection<String>>> tests = testIds.stream()
+			.map(id -> Pair.of(id, Stream.generate(() -> CommonUtils.namedId("step_")).limit(3).collect(Collectors.toList())))
+			.collect(Collectors.toList());
+
+	private final ListenerParameters parameters = TestUtils.standardParameters();
+	private final ReportPortalClient client = mock(ReportPortalClient.class);
+	private final ExecutorService executorService = Executors.newSingleThreadExecutor();
+	private final ReportPortal reportPortal = ReportPortal.create(client, parameters, executorService);
+
+	@BeforeEach
+	public void initLaunch() {
+		TestUtils.mockLaunch(client, launchId, suiteId, tests);
+		TestScenarioReporter.RP.set(reportPortal);
+		TestStepReporter.RP.set(reportPortal);
+	}
+
+	private static final Pair<String, String> FEATURE_CODE_REFERENCES = Pair.of("file:///",
+			"/agent-java-cucumber6/src/test/resources/features/belly.feature"
+	);
+
+	private static final Pair<String, String> SCENARIO_CODE_REFERENCES = Pair.of("file:///",
+			"/agent-java-cucumber6/src/test/resources/features/belly.feature"
+	);
+
+	@Test
+	public void verify_code_reference_scenario_reporter() {
+		TestUtils.runTests(BellyScenarioReporter.class);
+
+		verify(client, times(1)).startTestItem(any());
+		ArgumentCaptor<StartTestItemRQ> captor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(1)).startTestItem(same(suiteId), captor.capture());
+		verify(client, times(1)).startTestItem(same(testIds.get(0)), captor.capture());
+
+		List<StartTestItemRQ> items = captor.getAllValues();
+
+		StartTestItemRQ feature = items.get(0);
+		StartTestItemRQ scenario = items.get(1);
+
+		assertThat(feature.getDescription(),
+				allOf(notNullValue(), startsWith(FEATURE_CODE_REFERENCES.getKey()), endsWith(FEATURE_CODE_REFERENCES.getValue()))
+		);
+		assertThat(scenario.getDescription(),
+				allOf(notNullValue(), startsWith(SCENARIO_CODE_REFERENCES.getKey()), endsWith(SCENARIO_CODE_REFERENCES.getValue()))
+		);
+	}
+
+	@Test
+	public void verify_code_reference_step_reporter() {
+		TestUtils.runTests(BellyStepReporter.class);
+
+		ArgumentCaptor<StartTestItemRQ> captor = ArgumentCaptor.forClass(StartTestItemRQ.class);
+		verify(client, times(1)).startTestItem(captor.capture());
+		verify(client, times(1)).startTestItem(same(suiteId), captor.capture());
+		verify(client, times(3)).startTestItem(same(testIds.get(0)), any());
+
+		List<StartTestItemRQ> items = captor.getAllValues();
+		StartTestItemRQ feature = items.get(0);
+		StartTestItemRQ scenario = items.get(1);
+
+		assertThat(feature.getDescription(),
+				allOf(notNullValue(), startsWith(FEATURE_CODE_REFERENCES.getKey()), endsWith(FEATURE_CODE_REFERENCES.getValue()))
+		);
+		assertThat(scenario.getDescription(),
+				allOf(notNullValue(), startsWith(SCENARIO_CODE_REFERENCES.getKey()), endsWith(SCENARIO_CODE_REFERENCES.getValue()))
+		);
+	}
+}
