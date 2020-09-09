6a49ce4333eecbf7e1241f37ebc702ca92e8c3ad
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-09-09 15:49:29 +0300

    Copyrights added

--- src/test/java/com/epam/reportportal/cucumber/integration/callback/CallbackReportingSteps.java
+++ src/test/java/com/epam/reportportal/cucumber/integration/callback/CallbackReportingSteps.java
@@ -14,22 +14,7 @@
  * limitations under the License.
  */
 
-package com.epam.reportportal.cucumber.integration.feature;
+package com.epam.reportportal.cucumber.integration.callback;
 
-import io.cucumber.java.en.Given;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class AmbiguousSteps {
-	private static final Logger LOGGER = LoggerFactory.getLogger(AmbiguousSteps.class);
-
-	@Given("I have an ambiguous step (\\w+)")
-	public void i_have_an_ambiguous_step(String param) {
-		LOGGER.info("Inside 'I have an ambiguous step', parameter: " + param);
-	}
-
-	@Given("I have an ambiguous step two")
-	public void i_have_an_ambiguous_step_two() {
-		LOGGER.info("Inside 'I have an ambiguous step two'");
-	}
+public class CallbackReportingSteps {
 }
