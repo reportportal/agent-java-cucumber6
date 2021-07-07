8d20e76a83826228d8c8f88110f5e854220e11cd
Maurits Silvis <maurits.silvis@bequalified.de>
2021-07-07 14:32:32 +0200

    Get scenario names from test cases rather than from scenario contexts

--- src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
+++ src/main/java/com/epam/reportportal/cucumber/AbstractReporter.java
@@ -251,8 +251,8 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 	 * @param featureContext  current feature context
 	 * @param scenarioContext current scenario context
 	 */
-	protected void beforeScenario(RunningContext.FeatureContext featureContext, RunningContext.ScenarioContext scenarioContext) {
-		String scenarioName = Utils.buildName(scenarioContext.getKeyword(), AbstractReporter.COLON_INFIX, scenarioContext.getName());
+	protected void beforeScenario(RunningContext.FeatureContext featureContext, RunningContext.ScenarioContext scenarioContext, TestCase testCase) {
+		String scenarioName = Utils.buildName(scenarioContext.getKeyword(), AbstractReporter.COLON_INFIX, testCase.getName());
 		RunningContext.RuleContext rule = scenarioContext.getRule();
 		RunningContext.RuleContext currentRule = featureContext.getCurrentRule();
 		if (currentRule == null) {
@@ -709,7 +709,7 @@ public abstract class AbstractReporter implements ConcurrentEventListener {
 			return newScenarioContext;
 		});
 
-		beforeScenario(featureContext, scenarioContext);
+		beforeScenario(featureContext, scenarioContext, testCase);
 	}
 
 	protected void handleTestStepStarted(TestStepStarted event) {
